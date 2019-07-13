d79d27812775e5c790c21ed4b3fd0266b10f3a90
planetarydev <planetarydevelop@gmail.com>
2019-07-13 09:02:04 +0200

    Some cleanup

--- sql/helpers/functions/datetime/to_timestamp/to_timestamp.js
+++ sql/helpers/functions/datetime/to_timestamp/to_timestamp.js
@@ -2,25 +2,13 @@
 
 class toTimestamp extends SQLBuilder.SQLHelper {
 	constructor(sql){
-		// always call the construtor of SQLHelper
 		super(sql);
 
-		// declare the possible types that can be used
 		this.Types({
-			// $select: { first_name: { $left: 1 } }
-			// The value of $left is a Number
-			// <key-ident> = "first_name"
-			// <value-param> = 1
 			String: { syntax: this.Syntax('TO_TIMESTAMP(<value-param>)') },
-
-			// $select: { first_name: { $left: { $str: '~~first_name', $len: 1 } } }
-			// The value of $left is an Object: $left: { $str: '~~first_name', $len: 1 }
-			// <$str> = "~~first_name"
-			// <$len> = 1
 			Object: { syntax: this.Syntax('TO_TIMESTAMP(<$expr>, <$format>)', SQLBuilder.CALLEE) },
 		});
 
-		// define or register each Operator or Helper defined in the Syntax
 		this.$expr = new SQLBuilder.SQLPredefined.StringValueParam(sql);
 		this.$format = new SQLBuilder.SQLPredefined.StringValueParam(sql);
 	}
